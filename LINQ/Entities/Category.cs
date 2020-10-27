046bedf41faf7e7002689ad2692dca25dc914d80
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-27 20:34:34 -0300

    LINQ E LAMBDA

---
+++ LINQ/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LINQ.Entities
+{
+    public class Category
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
